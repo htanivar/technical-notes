57883c9272fc34e3ea1bf72e3edcf122cb316669
Ravi J <raviregi@gmail.com>
2026-03-12 18:32:07 +0530

    updated gh-ops.sh

--- 002_rsys/batch-scripts/sh/gh-ops.sh
+++ 002_rsys/batch-scripts/sh/gh-ops.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 set -e
 
 BRANCH=$(git branch --show-current)
 
 git push -u origin "$BRANCH"
 
-PR_URL=$(gh pr create --fill --head "$BRANCH")
+gh pr create --fill --head "$BRANCH"
 
-PR_NUMBER=$(echo "$PR_URL" | grep -o '[0-9]\+$')
-
-gh pr merge "$PR_NUMBER" --squash --delete-branch --auto
+gh pr merge --auto --squash --delete-branch
