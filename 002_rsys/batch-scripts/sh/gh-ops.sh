7e2094367cfc4e85f36b1b7f97b5ed5af95068c5
Ravi J <raviregi@gmail.com>
2026-03-12 18:33:45 +0530

    updated to get protected branch

--- 002_rsys/batch-scripts/sh/gh-ops.sh
+++ 002_rsys/batch-scripts/sh/gh-ops.sh
@@ -2,9 +2,19 @@
 set -e
 
 BRANCH=$(git branch --show-current)
+BASE=$(gh repo view --json defaultBranchRef --jq '.defaultBranchRef.name')
 
 git push -u origin "$BRANCH"
 
-gh pr create --fill --head "$BRANCH"
+gh pr create \
+  --base "$BASE" \
+  --head "$BRANCH" \
+  --title "$BRANCH" \
+  --body "auto pr" \
+  --repo "$(gh repo view --json nameWithOwner --jq '.nameWithOwner')"
 
-gh pr merge --auto --squash --delete-branch
+gh pr merge \
+  --squash \
+  --auto \
+  --delete-branch \
+  "$BRANCH"
